17d5aca3d81191ec7735dd29a47da660384be173
Canaan Owens <canaanowens@Canaans-MacBook-Pro-2021.local>
2022-12-06 11:32:59 -0800

    added bootstrap formatting for htmlcreation

--- index.js
+++ index.js
@@ -92,11 +92,57 @@ function another() {
     ])
     .then(({ more }) => {
       if (more) newEmployee();
-      else console.log(employees);
+      else renderHTMLFile();
     });
+  function renderHTMLFile() {
+    fs.writeFileSync(
+      "./index.html",
+      /*html*/ `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  </head>
+  <div class="jumbotron" style="text-align: center; border-bottom: 5px solid black; opacity : .9; background : linear-gradient(to top, rgb(96, 179, 220), rgb(239, 243, 245)); color:black; text-align:center; " id="jumbotron">
+    <h1 class="display-4" style="font-weight:bolder;">Team Roster</h1>
+    </div>
+    <div class="container">
+    <div class="row">
+  ${employees.map(
+    (employee) => /*html*/ `
+  <div class= "col-md-3 text-dark bg-light border border-dark rounded-lg" style = "margin : 5px;">
+    <h1 class="card-title">${employee.getName()}</h1>
+    <h2 class="card-subtitle mb-2 text-dark">${employee.getRole()}</h2>
+    <h3 class="card-subtitle mb-2 text-dark">Id: ${employee.getId()}</h3>
+    <h4 class="card-subtitle mb-2 text-muted"><a href="mailto:${employee.getEmail()}" class="card-link">${employee.getEmail()}</a></h3>
+    <h4 class="card-subtitle mb-2 text-muted"> ${uniqueField(employee)}</h3>
+  </div>
+  `
+  )} 
+    </div>
+          
+          `
+    );
+  }
 }
 
-function renderHTMLFile() {
-  fs.writeFileSync("./index.html");
+function uniqueField(employee) {
+  switch (employee.getRole()) {
+    case "Manager":
+      return `Office:${employee.getOfficeNumber()}`;
+
+    // ask for office number
+    case "Engineer":
+      //ask for git hub
+      return `<a href="https://github.com/${employee.getGithub()}" class="card-link">Github</a>`;
+
+    case "Intern":
+      // ask about school
+      return ` Student at: ${employee.getSchool()}`;
+  }
 }
+
 newEmployee();
